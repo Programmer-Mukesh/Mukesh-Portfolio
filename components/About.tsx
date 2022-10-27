8fa2f44c7133fc2e01c067f2c56f0e56a87b1599
Mukesh <mukeshkumar989898@gmail.com>
2022-10-27 13:52:45 +0530

    experience section added

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 const About = (props: Props) => {
   return (
-    <div className="relative h-screen flex flex-col text-center  md:text-left md:flex-row px-10 sm:px-5 justify-center items-center max-w-6xl mx-auto">
+    <div className="relative h-screen flex flex-col text-center  md:text-left md:flex-row px-5 justify-center items-center max-w-6xl mx-auto">
       <h3 className="absolute top-20 sm:top-24 uppercase tracking-[20px] text-2xl text-gray-500">
         About
       </h3>
@@ -29,7 +29,7 @@ const About = (props: Props) => {
         src="https://avatars.githubusercontent.com/u/77101515?s=400&u=adc13a6fd748f4e69f7eb0f304a55f5a7aee44e9&v=4"
       ></motion.img>
       <div className="space-y-10 px-0 md:px-10 ">
-        <h4 className="text-3xl font-semibold sm:text-4xl mt-4 md:mt-0">
+        <h4 className="text-2xl font-semibold sm:text-4xl mt-4 md:mt-0">
           Here is a{" "}
           <span className="underline decoration-[#F7AB0A]/40">little</span>{" "}
           background
@@ -37,9 +37,10 @@ const About = (props: Props) => {
         <p className="text-base xs:mt-0">
           I'm Mukesh, currently working as a Software Engineer-1 at Carnation
           InfoTech. I have more than 1 year of experience working on Front-end
-          Web Technologies like React, Redux, Next.js etc. I love designing web
-          pages and love creating unique designs. I also passionate about
-          photography and learning about new technologies.
+          Web Technologies like React, Redux, Next.js etc. I have completed my
+          B.Tech in Information Technology from IET lucknow in 2021. I love
+          designing web pages and love creating unique designs. I also
+          passionate about photography and learning new technologies.
         </p>
       </div>
     </div>
---
+++ components/WorkExperience.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ExperienceCard from "./ExperienceCard";
+
+type Props = {};
+
+const WorkExperience = (props: Props) => {
+  return (
+    <div className="relative h-screen flex flex-col text-center md:text-left md:flex-row px-10 sm:px-5 justify-evenly items-center max-w-6xl mx-auto">
+      <h3 className="absolute top-20 sm:top-24 uppercase tracking-[20px] text-2xl text-gray-500">
+        Experience
+      </h3>
+      <div className="flex w-full space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory justify-center">
+        <ExperienceCard />
+      </div>
+    </div>
+  );
+};
+
+export default WorkExperience;
