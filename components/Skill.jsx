3d31d86c23fd19112e09dfc61566147f8a2ec2f5
Mukesh <mukeshkumar989898@gmail.com>
2022-10-28 23:48:26 +0530

    skill section fixed

---
+++ components/Skill.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { motion } from "framer-motion";
+import useWindowDimensions from "../hooks/useWindowDimensions";
+
+const Skill = ({ directionLeft, logo, proficiency }) => {
+  const { height, width } = useWindowDimensions();
+
+  return (
+    <div className="group relative flex cursor-pointer">
+      <motion.img
+        initial={
+          width < 780
+            ? {
+                x: directionLeft ? -70 : 70,
+                opacity: 0,
+              }
+            : {
+                x: directionLeft ? -200 : 200,
+                opacity: 0,
+              }
+        }
+        whileInView={{
+          x: 0,
+          opacity: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+        className="rounded-full border border-gray-500 object-contain w-[60px] h-[60px] md:w-[70px] md:h-[70px]  filter group-hover:grayscale transition duration-300 ease-in-out"
+        src={logo}
+      />
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-[60px] h-[60px] md:w-[70px] md:h-[70px] rounded-full z-0">
+        <div className="flex items-center justify-center h-full">
+          <span className="text-xl font-bold text-black opacity-100">
+            {proficiency}%
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skill;
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -27,62 +27,77 @@ const Skills = () => {
         <Skill
           directionLeft={true}
           logo="https://seeklogo.com/images/H/html5-without-wordmark-color-logo-14D252D878-seeklogo.com.png"
+          proficiency="90"
         />
         <Skill
           directionLeft={true}
+          proficiency="80"
           logo="https://seeklogo.com/images/C/css-3-logo-023C1A7171-seeklogo.com.png"
         />
         <Skill
           directionLeft={true}
+          proficiency="80"
           logo="https://seeklogo.com/images/J/javascript-logo-8892AEFCAC-seeklogo.com.png"
         />
         <Skill
+          proficiency="80"
           logo="https://creazilla-store.fra1.digitaloceanspaces.com/icons/3254274/react-icon-md.png"
           directionLeft={true}
         />
 
         <Skill
+          proficiency="70"
           logo="https://seeklogo.com/images/R/redux-logo-9CA6836C12-seeklogo.com.png"
           directionLeft={true}
         />
         <Skill
+          proficiency="75"
           logo="https://seeklogo.com/images/N/next-js-logo-8FCFF51DD2-seeklogo.com.png"
           directionLeft={true}
         />
 
         <Skill
+          proficiency="70"
           directionLeft={true}
           logo="https://seeklogo.com/images/S/sass-logo-E41E7734A8-seeklogo.com.png"
         />
         <Skill
+          proficiency="80"
           logo="https://seeklogo.com/images/M/material-ui-logo-5BDCB9BA8F-seeklogo.com.png"
           directionLeft={true}
         />
         <Skill
+          proficiency="60"
           logo="https://seeklogo.com/images/G/git-logo-CD8D6F1C09-seeklogo.com.png"
           directionLeft={false}
         />
         <Skill
+          proficiency="60"
           logo="https://seeklogo.com/images/T/tailwind-css-logo-5AD4175897-seeklogo.com.png"
           directionLeft={false}
         />
         <Skill
+          proficiency="50"
           logo="https://seeklogo.com/images/T/typescript-logo-B29A3F462D-seeklogo.com.png"
           directionLeft={false}
         />
         <Skill
+          proficiency="80"
           logo="https://seeklogo.com/images/C/c-logo-43CE78FF9C-seeklogo.com.png"
           directionLeft={false}
         />
         <Skill
+          proficiency="80"
           logo="https://seeklogo.com/images/D/datocms-logo-7A13CD5713-seeklogo.com.png"
           directionLeft={false}
         />
         <Skill
+          proficiency="70"
           logo="https://seeklogo.com/images/R/react-native-logo-221C671C70-seeklogo.com.png"
           directionLeft={false}
         />
         <Skill
+          proficiency="70"
           logo="https://seeklogo.com/images/P/prismic-logo-F6A173E6D0-seeklogo.com.png"
           directionLeft={false}
         />
